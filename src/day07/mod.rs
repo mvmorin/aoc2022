85a1892c3a6449b2801252d11e1006fa23c13bba
Martin Morin <martin.morin@control.lth.se>
2022-12-08 18:55:11 +0100

    Day 7 rematch, doubly linked tree implementation
    Not a better way of doing it but at least I managed it in a sort of
    clean way. RefCells are weird to since we can break inmutabilty. Also
    added size caching for each directory.

--- src/day07/mod.rs
+++ src/day07/mod.rs
@@ -1,93 +1,154 @@
-use std::collections::HashMap;
-use std::collections::HashSet;
-use std::path::PathBuf;
-
 #[test]
 fn day07() {
     let input = include_str!("input.txt");
     // let input = include_str!("test_input.txt");
 
-    let dirs = parse_directories(input);
+    let mut dirs = parse_directories(input);
 
-    let all_sizes: Vec<usize> = dirs
-        .keys()
-        .map(|p| dir_size(p, &dirs))
-        .collect();
+    let tot_size = dirs
+        .iter_mut()
+        .map(|d| d.size())
+        .filter(|&v| v <= 100_000)
+        .sum::<usize>();
+    println!("{}", tot_size);
 
-    let tot_size_filtered = all_sizes.iter().filter(|&&v| v <= 100_000).sum::<usize>();
-    println!("{}", tot_size_filtered);
-
-    let size_to_delete = 30_000_000 - (70_000_000 - dir_size(&PathBuf::from("/"), &dirs));
-    let min_size_to_delete = all_sizes.iter().filter(|&&v| v >= size_to_delete).min().unwrap();
-    println!("{}", min_size_to_delete);
+    let size_to_delete = 30_000_000 - (70_000_000 - dirs[0].size()); // root happen to be at index 0
+    let min_possible_size = dirs
+        .iter_mut()
+        .map(|d| d.size())
+        .filter(|&v| v >= size_to_delete)
+        .min()
+        .unwrap();
+    println!("{}", min_possible_size);
 }
 
-#[derive(Debug, Eq, PartialEq, Hash)]
-struct File {
-    name: String,
-    size: usize,
-}
+fn parse_directories(commands: &str) -> Vec<Directory> {
+    let mut all_dirs = Vec::new();
 
-#[derive(Debug)]
-struct Directory {
-    files: HashSet<File>,
-    dirs: HashSet<PathBuf>,
-}
+    all_dirs.push(Directory::new("root".to_string()));
+    let mut cwd = all_dirs[0].clone();
 
-impl Directory {
-    fn new() -> Self {
-        Directory {
-            files: HashSet::new(),
-            dirs: HashSet::new(),
-        }
-    }
-}
-
-fn parse_directories(input: &str) -> HashMap<PathBuf, Directory> {
-    let mut cwd_path = PathBuf::from("/");
-
-    let mut dirs: HashMap<PathBuf, Directory> = HashMap::new();
-    dirs.insert(cwd_path.clone(), Directory::new());
-
-    for l in input.lines() {
+    for l in commands.lines() {
         if let Some(dir) = l.strip_prefix("$ cd ") {
             match dir {
-                "/" => { cwd_path = PathBuf::from("/"); },
-                ".." => { cwd_path.pop(); },
-                dir => { cwd_path.push(dir); },
+                "/" => { cwd = all_dirs[0].clone(); }
+                ".." => { cwd = cwd.get_parent(); }
+                dir => { cwd = cwd.get_subdir(dir); }
             }
         } else if l == "$ ls" {
             // do noting
         } else if let Some(dir) = l.strip_prefix("dir ") {
-            let mut new_path = cwd_path.clone();
-            new_path.push(dir);
+            let dir = Directory::new(dir.to_string());
 
-            let cwd = dirs.get_mut(&cwd_path).unwrap();
-            cwd.dirs.insert(new_path.clone());
-
-            dirs.entry(new_path.clone()).or_insert(Directory::new());
+            all_dirs.push(dir.clone());
+            cwd.add_subdir(dir.clone());
         } else {
             let mut parts = l.split(' ');
             let size = parts.next().unwrap().parse::<usize>().unwrap();
             let name = parts.next().unwrap().to_string();
             let file = File { name, size };
 
-            let cwd = dirs.get_mut(&cwd_path).unwrap();
-            cwd.files.insert(file);
+            cwd.add_file(file);
         }
     }
 
-    dirs
+    return all_dirs;
 }
 
-fn dir_size(path: &PathBuf, dirs: &HashMap<PathBuf, Directory>) -> usize {
-    let dir = match dirs.get(path) {
-        Some(dir) => dir,
-        None => panic!("can't find directory"),
-    };
 
-    let size_files = dir.files.iter().map(|f| f.size).sum::<usize>();
-    let size_dirs = dir.dirs.iter().map(|p| dir_size(p, dirs)).sum::<usize>();
+use std::rc::{Rc,Weak};
+use std::cell::{RefCell,Ref,RefMut};
+use std::collections::HashMap;
 
-    size_files + size_dirs
+#[derive(Debug,Clone)]
+struct Directory(Rc<RefCell<DirData>>);
+
+#[derive(Debug)]
+struct DirectoryWeak(Weak<RefCell<DirData>>);
+
+#[derive(Debug)]
+struct DirData {
+    name: String,
+    size: Option<usize>,
+    parent: DirectoryWeak,
+    files: HashMap<String,File>,
+    subdirs: HashMap<String,Directory>,
 }
+
+#[derive(Debug)]
+struct File {
+    name: String,
+    size: usize,
+}
+
+impl Directory {
+    fn new(name: String) -> Self {
+        let data = DirData {
+            name,
+            size: None,
+            parent: DirectoryWeak::new(),
+            files: HashMap::new(),
+            subdirs: HashMap::new(),
+        };
+        Directory(Rc::new(RefCell::new(data)))
+    }
+
+    fn get_data(&self) -> Ref<DirData> {
+        self.0.borrow()
+    }
+
+    fn get_data_mut(&mut self) -> RefMut<DirData> {
+        self.0.borrow_mut()
+    }
+
+    fn to_weak(&self) -> DirectoryWeak {
+        DirectoryWeak(Rc::downgrade(&self.0))
+    }
+
+    fn add_subdir(&mut self, mut dir: Directory) {
+        let key = dir.get_data().name.to_string();
+        dir.get_data_mut().parent = self.to_weak();
+        self.get_data_mut().subdirs.entry(key).or_insert(dir);
+    }
+
+    fn get_subdir(&self, name: &str) -> Directory {
+        self.get_data().subdirs.get(name).unwrap().clone()
+    }
+
+    fn get_parent(&self) -> Directory {
+        self.get_data().parent.to_strong()
+    }
+
+    fn add_file(&mut self, file: File) {
+        let key = file.name.to_string();
+        self.get_data_mut().files.entry(key).or_insert(file);
+    }
+
+    fn size(&mut self) -> usize {
+        let mut data = self.get_data_mut();
+
+        match data.size {
+            Some(size) => size,
+            None => {
+                let size_files = data.files.values().map(|f| f.size).sum::<usize>();
+                let size_dirs = data.subdirs.values_mut().map(|d| d.size()).sum::<usize>();
+                let size = size_files + size_dirs;
+                data.size = Some(size);
+
+                size
+            }
+        }
+
+    }
+}
+
+impl DirectoryWeak {
+    fn to_strong(&self) -> Directory {
+        Directory(self.0.upgrade().unwrap().clone())
+    }
+
+    fn new() -> Self {
+        DirectoryWeak(Weak::new())
+    }
+}
+
