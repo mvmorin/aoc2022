aa7c5b12156c607f9e6f6b249c1819176c4dab94
Martin Morin <martin.morin@control.lth.se>
2022-12-14 14:20:35 +0100

    Change to vector on day14

--- src/day14/mod.rs
+++ src/day14/mod.rs
@@ -1,6 +1,6 @@
-use std::collections::HashSet;
+// use std::collections::HashSet;
 
-type Map = HashSet<(i32,i32)>;
+type Map = Vec<Vec<bool>>;
 
 #[test]
 fn day14() {
@@ -9,43 +9,48 @@ fn day14() {
     // println!("{}", input);
 
     // part 1
-    let mut map = parse_input(input);
-    let lowest = find_lowest(&map);
+    let (mut map, lowest) = parse_input(input);
     let mut count = 0;
-    while add_sand_no_floor(&mut map, (500,0), lowest) {
+    while add_sand_no_floor(&mut map, lowest) {
         count += 1;
     }
     println!("{:?}", count);
 
     // part 2
-    let mut map = parse_input(input);
-    let lowest = find_lowest(&map);
+    let (mut map, lowest) = parse_input(input);
     let mut count = 0;
-    while add_sand_floor(&mut map, (500,0), lowest) {
+    while add_sand_floor(&mut map, lowest) {
         count += 1;
     }
     println!("{:?}", count);
 }
 
-fn parse_input(input: &str) -> Map {
-    let mut map = HashSet::new();
+fn parse_input(input: &str) -> (Map, usize) {
+    let mut map = Vec::new();
+    for _ in 0..=1000 { // size is based on starting point
+        map.push(vec![false; 500]);
+    }
+
+    let mut lowest = 0;
 
     for line in input.lines() {
         let mut points = line.split(" -> ");
         let (mut col_p, mut row_p) = parse_tuple(points.next().unwrap());
-        map.insert((col_p,row_p));
+        map[col_p][row_p] = true;
+        lowest = lowest.max(row_p);
 
         for s in points {
             let (col,row) = parse_tuple(s);
 
-            let c_diff = col - col_p;
-            let r_diff = row - row_p;
+            let c_diff = col as isize - col_p as isize;
+            let r_diff = row as isize - row_p as isize;
             let n_steps = (c_diff.abs()).max(r_diff.abs());
 
             for step in 1..=n_steps {
-                let col_mid = col_p + step * c_diff.signum();
-                let row_mid = row_p + step * r_diff.signum();
-                map.insert((col_mid,row_mid));
+                let col_mid = (col_p as isize + step * c_diff.signum()) as usize;
+                let row_mid = (row_p as isize + step * r_diff.signum()) as usize;
+                map[col_mid][row_mid] = true;
+                lowest = lowest.max(row_mid);
             }
 
             col_p = col;
@@ -53,64 +58,60 @@ fn parse_input(input: &str) -> Map {
         }
     }
 
-    return map
+    return (map, lowest)
 }
 
-fn parse_tuple(s: &str) -> (i32,i32) {
+fn parse_tuple(s: &str) -> (usize,usize) {
     let mut coords = s.split(',');
-    let col = coords.next().unwrap().parse::<i32>().unwrap();
-    let row = coords.next().unwrap().parse::<i32>().unwrap();
+    let col = coords.next().unwrap().parse::<usize>().unwrap();
+    let row = coords.next().unwrap().parse::<usize>().unwrap();
     (col,row)
 }
 
-fn find_lowest(map: &Map) -> i32 {
-    map.iter().map(|k| k.1).max().unwrap()
-}
-
-fn add_sand_no_floor(map: &mut Map, start_point: (i32,i32), lowest: i32) -> bool {
-    if map.contains(&start_point) {
+fn add_sand_no_floor(map: &mut Map, lowest: usize) -> bool {
+    if map[500][0] {
         return false;
     }
 
-    let (mut col, mut row) = start_point;
+    let (mut col, mut row) = (500,0);
     loop {
         if row >= lowest {return false;}
 
-        if !map.contains(&(col,row+1)) {
+        if !map[col][row+1] {
             row += 1;
-        } else if !map.contains(&(col-1,row+1)) {
+        } else if !map[col-1][row+1] {
             row += 1;
             col -= 1;
-        } else if !map.contains(&(col+1,row+1)) {
+        } else if !map[col+1][row+1] {
             row += 1;
             col += 1;
         } else {
-            map.insert((col,row));
+            map[col][row] = true;
             return true;
         }
     }
 }
 
-fn add_sand_floor(map: &mut Map, start_point: (i32,i32), lowest: i32) -> bool {
-    if map.contains(&start_point) {
+fn add_sand_floor(map: &mut Map, lowest: usize) -> bool {
+    if map[500][0] {
         return false;
     }
 
-    let (mut col, mut row) = start_point;
+    let (mut col, mut row) = (500,0);
     loop {
         if row + 1 == lowest + 2 {
-            map.insert((col,row));
+            map[col][row] = true;
             return true;
-        } else if !map.contains(&(col,row+1)) {
+        } else if !map[col][row+1] {
             row += 1;
-        } else if !map.contains(&(col-1,row+1)) {
+        } else if !map[col-1][row+1] {
             row += 1;
             col -= 1;
-        } else if !map.contains(&(col+1,row+1)) {
+        } else if !map[col+1][row+1] {
             row += 1;
             col += 1;
         } else {
-            map.insert((col,row));
+            map[col][row] = true;
             return true;
         }
     }
